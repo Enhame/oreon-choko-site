f937f2461ca2c806fed19baac743e64fb973b42d
Mikhail Kuznetsov <enhame@inbox.ru>
2022-06-19 00:07:40 +0400

    basic

---
+++ oreon-choco-site/apps/oreon-choco-site/src/app/config/firebase.js
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+//
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCT_etN0jl6uB-_G2wDPrcCaTfGbPC0P0A",
+//   authDomain: "oreon-choco-site.firebaseapp.com",
+//   projectId: "oreon-choco-site",
+//   storageBucket: "oreon-choco-site.appspot.com",
+//   messagingSenderId: "1045344715860",
+//   appId: "1:1045344715860:web:c9d663461fc3c8d9f619c1",
+//   measurementId: "G-EYRR2NRZQ8"
+// };
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
